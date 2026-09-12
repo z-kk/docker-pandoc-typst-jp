a198321061c70cc45b572446884f94515970791b
z-kk <z-kk@outlook.jp>
2026-09-12 23:22:19 +0900

    update compile_example.sh

--- compile_example.sh
+++ compile_example.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+if [ -e data ]; then
+    echo "rm ./data before run this script"
+    exit 1
+fi
+
 ln -s examples data
 docker compose run --user $(id -u):$(id -g) --rm app -o sample.pdf sample.md
 rm data
